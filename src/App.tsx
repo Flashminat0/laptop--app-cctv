9902ca809f54d4b58d20b75d164280a9db3b5ae1
Flashminat0 <wyasaslowe@gmail.com>
2023-09-04 09:29:48 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [scanMode, setScanMode] = useState<boolean>(false);
+    const [showPattern, setShowPattern] = useState<boolean>(true);
+    const [arrayOfSix, setArrayOfSix] = useState<string[]>([]);
+    const [backGroundColor, setBackGroundColor] = useState<string>("lime");
+
+    const generateArray = () => {
+        for (let i = 0; i < 6; i++) {
+            const randomBoolean = Math.random() >= 0.5;
+
+            if (randomBoolean) {
+                setArrayOfSix((prev) => [...prev, "1"]);
+            } else {
+                setArrayOfSix((prev) => [...prev, "0"]);
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (arrayOfSix.length !== 6) {
+            generateArray();
+        }
+    }, []);
+
+
+    useEffect(() => {
+        console.log(arrayOfSix)
+    }, [arrayOfSix]);
+
+    return (
+        <div className={`w-screen h-screen`} onClick={() => {
+            setShowPattern(!showPattern)
+        }}>
+            {showPattern ? <>
+                <div className={`w-screen h-screen grid grid-cols-3 grid-rows-2`}>
+                    {arrayOfSix.map((item, index) => {
+                        return (
+                            <div key={index}
+                                 className={`w-full h-full ${item === "1" ? "bg-black" : "bg-white"}`}>.</div>
+                        )
+                    })}
+                </div>
+            </> : <>
+                <div className={`w-screen h-screen`}
+                 style={{backgroundColor: backGroundColor}}
+                >
+
+                </div>
+            </>}
+
+
+        </div>
+    );
 }
 
 export default App;
